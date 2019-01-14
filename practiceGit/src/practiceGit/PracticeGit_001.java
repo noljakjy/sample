01a83297e3407413e267474500b83e2f4aafeeab
jyk <jyk@192.168.2.112>
2019-01-15 01:34:31 +0900

    dddd

--- practiceGit/src/practiceGit/PracticeGit_001.java
+++ practiceGit/src/practiceGit/PracticeGit_001.java
@@ -6,6 +6,7 @@ public class PracticeGit_001 {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("helloworld");
 		System.out.println("helloworld");
+		System.out.println("master branch");
 		System.out.println("sub1 barnch");
 	}
 
